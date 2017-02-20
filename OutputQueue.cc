5bd340c476a3e261139fcf871d1987d4b5c100d4
Leona Xiong <leonaxiongxin@163.com>
2017-02-20 10:29:13 +0800

    Create OutputQueue.cc

---
+++ OutputQueue.cc
@@ -0,0 +1,21 @@
+int count(struct sk_buff* skb)
+{
+	struct iphdr *iph; //IP header
+	struct tcphdr *tcph; //TCP header
+	//struct udphdr *udph; //UDP header
+	struct timeval tv; //time struct(second & micro second)
+	static int totalpacket=0;
+	do_gettimeofday(&tv); //get now time
+	_kernel_seconds_t outtime=(tv.tv_sec*1000000)+tv.tv_usec; // intime = micro second
+	iph = ip_hdr(skb); //get IP header
+
+	if(iph->protocal==6){ //TCP packet
+		tcph=(struct tcphdr *)((char *)iph+(iph->ihl*4));
+		if(ntohs(tcph->source) == 8888) // TCP header, port number = 8888(change to your port number)
+		{
+			totalpacket++;
+			printk("Socket_i\t%ld\t%d\t%d\n",outtime,ntohs(iph-> tot_len),totalpacket); // time, total length, total packet
+		}
+	}	
+	return 0;
+}
